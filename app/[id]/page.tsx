b7d15be9d2e4194e556efae6463bf4246391ab8a
nicolasgodineau <nicolasgodineau@live.fr>
2024-08-10 15:33:28 -0400

    mise en place de l'extraction du titre

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -5,11 +5,21 @@ import { Metadata } from "next";
 export default async function Page({ params }: { params: { id: string } }) {
     const pageId = params.id;
     const page = await getPage(pageId);
-    console.log("page:", page);
+
+    const extractTitle = (property: any): string => {
+        // Assurer que la propriété est de type title
+        if (property?.type === "title" && property.title.length > 0) {
+            console.log("property:", property);
+            return property.title[0]?.plain_text || "Sans titre";
+        }
+        return "Sans titre";
+    };
 
     return (
         <div>
             <h1>Recipe page {page.id}</h1>
+
+            <p>{extractTitle(page.properties.Nom)}</p>
         </div>
     );
 }
